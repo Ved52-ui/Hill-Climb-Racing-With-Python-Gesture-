d40d4100e9c08f1d84d2c87304f8c8c272e4ecc5
Ved Patel <137433618+Ved52-ui@users.noreply.github.com>
2025-08-13 19:22:50 +0530

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 import mediapipe as mp 
 from pynput.keyboard import Key, Controller 
-#vfkvokvvrkvmrrmvlrrvkr vvvkrvrov vmrrkvkrrvk rkvrrkvrkrv jrifjejivhrir gjrggriggj j;sjiv 
+#vfkvokvvrkvmrrmvlrrvkr vvvkrvrov vmrrkvkrrvk rkvrrkvrkrv jrifjejivhrir gjrggriggj j;sji
 # Controlling The Keyboard
 keyboard = Controller()
 
