db5849cedd68979b6ad50277e9b21ec67ccd9f6f
Ved Patel <137433618+Ved52-ui@users.noreply.github.com>
2025-08-23 11:01:45 +0530

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,4 +1,4 @@
-ijiimvkkfgbfiiffjfkddlsurrllhtnnuivoddndldngjshhfbgkdngkdngkfn
+jiimvkkfgbfiiffjfkddlsurrllhtnnuivoddndldngjshhfbgkdngkdngkfn
 import cv2 as cv
 import mediapipe as mp 
 from pynput.keyboard import Key, Controller 
